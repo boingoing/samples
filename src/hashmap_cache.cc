5dafe63bd3a32ec051e7e108d4e11d3855376388
Taylor Woll <taylor.woll@gmail.com>
2023-09-06 19:21:15 -0700

    Improve performance of slot container and lru functionality in the cache

--- src/hashmap_cache.cc
+++ src/hashmap_cache.cc
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <list>
 #include <map>
 #include <mutex>
 #include <unordered_map>
@@ -10,65 +11,131 @@
 #include "hashmap_cache.h"
 #include "helpers.h"
 
+template <typename KeyType>
 class ReplacementAlgorithmBase {
  public:
-  virtual void NotifyUsed(size_t slot_index) = 0;
-  virtual size_t ChooseEvictionTarget() = 0;
+  virtual void NotifyUsed(KeyType key) = 0;
+  virtual const KeyType& ChooseEvictionTarget() const = 0;
+  virtual void RemoveKey(const KeyType& key) = 0;
 };
 
-class LeastRecentlyUsedReplacementAlgorithm : ReplacementAlgorithmBase {
+template <typename KeyType>
+class LeastRecentlyUsedReplacementAlgorithm : ReplacementAlgorithmBase<KeyType> {
  private:
-  size_t age_counter_ = 0;
-  std::vector<size_t> age_bits_;
+  std::list<KeyType> usage_tracker_;
+  std::unordered_map<KeyType, typename std::list<KeyType>::iterator> list_node_map_;
 
  public:
-  LeastRecentlyUsedReplacementAlgorithm() = delete;
-
-  LeastRecentlyUsedReplacementAlgorithm(size_t num_slots) : age_bits_(num_slots) {
+  void NotifyUsed(KeyType key) override {
+    RemoveKey(key);
+    usage_tracker_.push_front(key);
+    list_node_map_[key] = usage_tracker_.begin();
   }
 
-  ~LeastRecentlyUsedReplacementAlgorithm() = default;
-
-  void NotifyUsed(size_t slot_index) override {
-    age_bits_[slot_index] = age_counter_++;
+  const KeyType& ChooseEvictionTarget() const override {
+    return usage_tracker_.back();
   }
 
-  size_t ChooseEvictionTarget() override {
-    auto index_age = age_bits_[0];
-    size_t index = 0;
-    for (size_t i = 1; i < age_bits_.size(); i++) {
-      if (age_bits_[i] < index_age) {
-        index = i;
-        index_age = age_bits_[i];
-      }
+  void RemoveKey(const KeyType& key) override {
+    const auto& iter = list_node_map_.find(key);
+
+    if (iter != list_node_map_.cend()) {
+      usage_tracker_.erase(iter->second);
+      list_node_map_.erase(iter);
     }
-    return index;
   }
 };
 
-template <typename KeyType, typename ValueType, typename ReplacementAlgorithm = LeastRecentlyUsedReplacementAlgorithm>
+template <typename KeyType, typename ValueType, typename ReplacementAlgorithm = LeastRecentlyUsedReplacementAlgorithm<KeyType>>
+class CacheSet {
+ private:
+  size_t capacity_;
+  std::unordered_map<KeyType, ValueType> map_store_;
+  ReplacementAlgorithm replacement_algorithm_;
+  std::mutex mutex_;
+
+ public:
+  CacheSet() = delete;
+  CacheSet(const CacheSet&) = delete;
+  CacheSet(CacheSet&&) noexcept {}
+
+  CacheSet(size_t capacity) : capacity_(capacity) {}
+
+  ~CacheSet() = default;
+
+  ValueType Get(KeyType key) {
+    std::lock_guard guard(mutex_);
+    auto& iter = map_store_.find(key);
+
+    if (iter != map_store_.cend()) {
+      replacement_algorithm_.NotifyUsed(key);
+      return iter->second;
+    }
+
+    return {};
+  }
+
+  void Set(KeyType key, ValueType value) {
+    std::lock_guard guard(mutex_);
+    auto& iter = map_store_.find(key);
+
+    if (iter != map_store_.end()) {
+      std::cout << " Updating key \"" << key << "\" from \"" << iter->second << "\" to \"" << value << "\"" << std::endl;
+      iter->second = value;
+    } else {
+      if (map_store_.size() == capacity_) {
+        std::cout << " Conflict miss. Determining which key to replace..." << std::endl;
+        const auto& key_to_evict = replacement_algorithm_.ChooseEvictionTarget();
+
+        const auto& iter = map_store_.find(key_to_evict);
+        std::cout << "  Evicting key \"" << iter->first << "\" => \"" << iter->second << "\" ";
+        std::cout << "and setting key \"" << key << "\" => \"" << value << "\"" << std::endl;
+
+        RemoveKey(key_to_evict);
+      } else {
+        std::cout << " Setting new key \"" << key << "\" => \"" << value << "\"" << std::endl;
+      }
+
+      map_store_[key] = value;
+    }
+
+    replacement_algorithm_.NotifyUsed(key);
+  }
+
+  void Print() const {
+    bool is_first = true;
+    std::cout << "[ ";
+    for (const auto& m : map_store_) {
+      if (!is_first) {
+        std::cout << "| ";
+      }
+      is_first = false;
+      std::cout << m.first << " => " << m.second << " ";
+    }
+    std::cout << "]" << std::endl;
+  }
+
+ private:
+  void RemoveKey(const KeyType& key) {
+    const auto& iter = map_store_.find(key);
+
+    if (iter != map_store_.cend()) {
+      replacement_algorithm_.RemoveKey(key);
+      map_store_.erase(iter);
+    }
+  }
+};
+
+template <typename KeyType, typename ValueType, typename ReplacementAlgorithm = LeastRecentlyUsedReplacementAlgorithm<KeyType>>
 class SetAssociativeCache {
  private:
-  struct Slot {
-    KeyType key;
-    ValueType value;
-  };
-
-  struct SlotContainer : public std::vector<Slot> {
-    ReplacementAlgorithm replacement_algorithm_;
-
-    SlotContainer() = delete;
-    SlotContainer(size_t num_slots) : replacement_algorithm_(num_slots) {}
-    ~SlotContainer() = default;
-  };
-
   size_t num_sets_;
   size_t num_slots_;
-  std::vector<SlotContainer> slots_;
-  std::mutex slot_mutex_;
+  std::vector<CacheSet<KeyType, ValueType, ReplacementAlgorithm>> slots_;
 
  public:
   SetAssociativeCache() = delete;
+  SetAssociativeCache(const SetAssociativeCache&) = delete;
 
   SetAssociativeCache(size_t num_sets, size_t num_slots) : num_sets_(num_sets), num_slots_(num_slots) {
     Reallocate();
@@ -78,83 +145,31 @@ class SetAssociativeCache {
 
   void Set(KeyType key, ValueType value) {
     const auto set_index = HashToSet(key);
-    std::lock_guard guard(slot_mutex_);
-    auto& container = slots_[set_index];
-
     std::cout << "Attempting to set key \"" << key << "\" => \"" << value << "\" in bucket " << set_index << "..." << std::endl;
-
-    // Check to see if key is already mapped.
-    for (size_t i = 0; i < container.size(); i++) {
-      auto& slot = container[i];
-      if (slot.key == key) {
-        std::cout << " Updating key \"" << key << "\" from \"" << slot.value << "\" to \"" << value << "\"" << std::endl;
-
-        slot.value = value;
-        container.replacement_algorithm_.NotifyUsed(i);
-        return;
-      }
-    }
-
-    // If we haven't filled the container, add a new slot for this key at the back.
-    if (container.size() < num_slots_) {
-      std::cout << " Setting new key \"" << key << "\" => \"" << value << "\"" << std::endl;
-
-      container.replacement_algorithm_.NotifyUsed(container.size());
-      container.push_back({key, value});
-      return;
-    }
-
-    // The container is full. Evict something.
-    std::cout << " Conflict miss. Determining which key to replace..." << std::endl;
-
-    const auto index = container.replacement_algorithm_.ChooseEvictionTarget();
-    container.replacement_algorithm_.NotifyUsed(index);
-    auto& evicted_slot = container[index];
-
-    std::cout << "  Evicting key \"" << evicted_slot.key << "\" => \"" << evicted_slot.value << "\" ";
-    std::cout << "and setting key \"" << key << "\" => \"" << value << "\"" << std::endl;
-
-    evicted_slot.key = key;
-    evicted_slot.value = value;
+    auto& container = slots_[set_index];
+    return container.Set(key, value);
   }
 
   ValueType Get(KeyType key) {
     const auto set_index = HashToSet(key);
     auto& container = slots_[set_index];
-
-    for (size_t i = 0; i < container.size(); i++) {
-      auto& slot = container[i];
-      if (slot.key == key) {
-        container.replacement_algorithm_.NotifyUsed(i);
-        return slot.value;
-      }
-    }
-
-    return {};
+    return container.Get(key);
   }
 
-  void Print() {
-    for (size_t i = 0; i < slots_.size(); i++) {
-      std::cout << "Set " << i << " = [ ";
-      const auto& container = slots_[i];
-      for (size_t j = 0; j < container.size(); j++) {
-        const auto& slot = container[j];
-        if (j != 0) {
-          std::cout << "| ";
-        }
-        std::cout << slot.key << " => " << slot.value << " ";
-      }
-      std::cout << "]" << std::endl;
+  void Print() const {
+    size_t i = 0;
+    for (const auto& c : slots_) {
+      std::cout << "Set " << i++ << " = ";
+      c.Print();
     }
   }
 
  private:
   void Reallocate() {
-    std::lock_guard guard(slot_mutex_);
     slots_.clear();
     slots_.reserve(num_sets_);
     for (size_t i = 0; i < num_sets_; i++) {
-      slots_.emplace_back(SlotContainer(num_slots_));
+      slots_.emplace_back(num_slots_);
     }
   }
 
@@ -166,7 +181,7 @@ class SetAssociativeCache {
 int main_cache() {
   std::cout << std::endl << "Testing SetAssociativeCache..." << std::endl;
 
-  SetAssociativeCache<int, int> cache(4,5);
+  SetAssociativeCache<int, int> cache(4, 5);
   cache.Set(1, 2);
   cache.Set(5, 3);
   cache.Set(9, 4);
