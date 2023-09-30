17d5a9e50c70f4629582d069c43cb4f8dbb9f1da
Taylor Woll <taylor.woll@gmail.com>
2023-09-29 22:36:02 -0700

    Add fastest segment among trackpoints with/without gaps implementation

--- src/math_intervals.cc
+++ src/math_intervals.cc
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <map>
 #include <vector>
 
 #include "test/TestCase.h"
@@ -157,6 +158,129 @@ TEST_CASE_WITH_DATA(IntervalSetTest, tests, IntervalSetTestData, interval_set_te
   }
 
   const auto result = set.isInSet(data.x);
+  trace << "Result: " << result << " (expected: " << data.expected << ")" << std::endl;
+  assert.equal(result, data.expected);
+}
+
+// A single datapoint sampled from the hardware GPS indicating at |time|,
+// the user traveled |distance| units.
+struct Trackpoint {
+  int time;
+  int distance;
+
+  friend std::ostream& operator<<(std::ostream& os, const Trackpoint& t) {
+    return os << "[" << t.time << ", " << t.distance << "]";
+  }
+};
+
+// A set of trackpoints which, taken together, form the data making up a
+// physical activity over time.
+struct Activity {
+  std::string id;
+  std::string date;
+  std::vector<Trackpoint> trackpoints;
+};
+
+// Find the fastest segment over the course of the activity during which the
+// user traveled |benchmark_distance| units.
+// Returns the time taken to travel |benchmark_distance| units.
+// For this implementation, the trackpoints in |a| are cumulative and ordered
+// by time with each trackpoint separated by a 10 unit increment.
+int fastest_segment(const Activity& a, int benchmark_distance) {
+  // Default to the largest value - ideally use numerical_limits
+  int current_fastest = 10000;
+  // There are no gaps in the trackpoints, each one is exactly 10 distance
+  // units from the next.
+  // This means trackpoint[3] must have distance field set to 30.
+  // The distance of the segment we're interested in is, thus,
+  // (benchmark_distance / 10) trackpoints.
+  const int segment_length_in_trackpoints = benchmark_distance / 10;
+
+  // Consider each segment within the set of trackpoints.
+  for (size_t i = 0; i < a.trackpoints.size() - segment_length_in_trackpoints; i++) {
+    const size_t end_index = i + segment_length_in_trackpoints;
+    // Time taken for this segment is the delta of time fields.
+    const int segment_delta = a.trackpoints[end_index].time - a.trackpoints[i].time;
+
+    // Lower |segment_delta| indicates higher speed of the segment because
+    // all segments we're considering are the same width in distance units.
+    current_fastest = std::min(segment_delta, current_fastest);
+  }
+
+  return current_fastest;
+}
+
+// Find the fastest segment over the course of the activity during which the
+// user traveled |benchmark_distance| units.
+// Returns the time taken to travel |benchmark_distance| units.
+// For this implementation, the trackpoints in |a| are cumulative and ordered
+// by time with all trackpoints located at 10 unit increments. Not all
+// trackpoints are present in the activity - there may be gaps.
+int fastest_segment_with_gaps(const Activity& a, int benchmark_distance) {
+  int current_fastest = 10000;
+  const int segment_length_in_trackpoints = benchmark_distance / 10;
+
+  // Compute a map of expected trackpoint index (ie: trackpiont at distance
+  // 20 should be at index 2) to the actual index of the trackpoint in the
+  // vector. We use this to locate trackopint at distance 20 when the trackpoint
+  // at distance 10 is missing, for example.
+  std::map<int, int> trackpoint_index_map;
+  for (size_t i = 0; i < a.trackpoints.size(); i++) {
+    const auto expected_idx = a.trackpoints[i].distance / 10;
+    trackpoint_index_map[expected_idx] = i;
+  }
+
+  const int max_expected_index = a.trackpoints[a.trackpoints.size() - 1].distance / 10 + 1;
+  for (size_t i = 0; i < max_expected_index - segment_length_in_trackpoints; i++) {
+    const auto iter = trackpoint_index_map.find(i);
+    if (iter == trackpoint_index_map.cend()) {
+      continue;
+    }
+
+    const auto start_actual_index = iter->second;
+    const int expected_end_index = i + segment_length_in_trackpoints;
+    const auto iter2 = trackpoint_index_map.find(expected_end_index);
+    if (iter2 == trackpoint_index_map.cend()) {
+      continue;
+    }
+
+    const auto end_actual_index = iter2->second;
+    const int segment_delta = a.trackpoints[end_actual_index].time - a.trackpoints[start_actual_index].time;
+    current_fastest = std::min(segment_delta, current_fastest);
+  }
+
+  return current_fastest;
+}
+
+struct ActivityTrackpointTestData : TestCaseDataWithExpectedResult<int> {
+  Activity activity;
+  int benchmark_distance;
+};
+
+std::vector<ActivityTrackpointTestData> activity_trackpoint_tests = {
+  {5, {"", "", {{0, 0}, {4, 10}, {9, 20}, {15, 30}, {18, 40}, {22, 50}, {23, 60}}}, 20},
+};
+
+class ActivityTrackpointTest : public TestCase {};
+
+TEST_CASE_WITH_DATA(ActivityTrackpointTest, fastest_segment, ActivityTrackpointTestData, activity_trackpoint_tests) {
+  trace << std::endl << "Finding the fastest segment of " << data.benchmark_distance << " distance units for this set of trackpoints:" << std::endl;
+  trace.vector(data.activity.trackpoints);
+
+  const auto result = fastest_segment(data.activity, data.benchmark_distance);
+  trace << "Result: " << result << " (expected: " << data.expected << ")" << std::endl;
+  assert.equal(result, data.expected);
+}
+
+std::vector<ActivityTrackpointTestData> activity_trackpoint_with_gaps_tests = {
+  {7, {"", "", {{0, 0}, {4, 10}, {9, 20}, {18, 40}, {31, 70}, {34, 80}, {38, 90}}}, 20},
+};
+
+TEST_CASE_WITH_DATA(ActivityTrackpointTest, fastest_segment_with_gaps, ActivityTrackpointTestData, activity_trackpoint_with_gaps_tests) {
+  trace << std::endl << "Finding the fastest segment of " << data.benchmark_distance << " distance units for this set of trackpoints which may contain gaps:" << std::endl;
+  trace.vector(data.activity.trackpoints);
+
+  const auto result = fastest_segment_with_gaps(data.activity, data.benchmark_distance);
   trace << "Result: " << result << " (expected: " << data.expected << ")" << std::endl;
   assert.equal(result, data.expected);
 }
